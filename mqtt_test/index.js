e971ed532270f4a064ab4d3985b2c43fc4a7d10d
koroshi <stlcxc@163.com>
2018-05-02 22:46:24 +0800

    mqtt xuexi

---
+++ mqtt_test/index.js
@@ -0,0 +1,21 @@
+var mqtt = require('mqtt')
+
+let mqttConfig = {
+    "host": "test.mosquitto.org",
+    "port": "1883",
+    "clientId": "groupid@@@deviceid",
+    // "username": "xxxx",
+    // "password": "xxx"
+}
+var client = mqtt.connect(mqttConfig)
+client.on('connect', function () {
+    client.subscribe('test/testAC')
+    client.publish('test/testAC', 'Hello mqtt1')
+})
+
+client.on('message', function (topic, message) {
+    // message is Buffer
+    console.log(topic)
+    console.log(message.toString())
+    client.end()
+})
