c04d04a14a33ee611aa235773886e0611619fbcd
koroshi <stlcxc@163.com>
2019-02-27 17:37:13 +0800

    feat:demo1-6

--- graphql/graphqlDamoApi/server.js
+++ graphql/graphqlDamoApi/server.js
@@ -2,37 +2,236 @@ const express = require("express");
 const graphqlHttp = require("express-graphql");
 const {buildSchema} = require('graphql');
 
-let schema = buildSchema(`
-	type Query{
-		hello:String
-	}
-`);
-
-let schemaVar = buildSchema(`
-type Query {
-	rollDice(numDice: Int!, numSides: Int): [Int]
-  }
-`);
-
-let root = {
-	hello:()=>{
-		return 'Hello World';
-	}
-};
 
 let app = express();
 
-app.use('/graphql', graphqlHttp({
-	schema:schema,
-	rootValue:root,
-	graphiql:true
-}));
+//demo0
+{
+	let schema = buildSchema(`
+		type Query{
+			hello:String
+		}
+	`);
 
-app.use('/graphqlvar', graphqlHttp({
-	schema:schemaVar,
-	rootValue:root,
-	graphiql:true
-}));
+
+
+	let root = {
+		hello:()=>{
+			return 'Hello World';
+		}
+	};
+
+	app.use('/graphql', graphqlHttp({
+		schema:schema,
+		rootValue:root,
+		graphiql:true
+	}));
+}
+
+//demo1
+{
+	let schemaVar = buildSchema(`
+	type Query {
+		rollDice(numDice: Int!, numSides: Int): [Int]
+	}
+	`);
+	let root1 = {
+		hello:()=>{
+			return 'Hello World';
+		}
+	};
+	app.use('/graphqlvar', graphqlHttp({
+		schema:schemaVar,
+		rootValue:root1,
+		graphiql:true
+	}));
+}
+
+//demo2
+{
+	let schemaTypes = buildSchema(`
+		type Query{
+			quoteOfTheDay:String
+			random:Float!
+			rollThreeDice:[Int]
+		}
+	`);
+
+	let rootTypes = {
+		quoteOfTheDay:()=>{
+			return Math.random() < 0.5 ? 'Take it easy' : 'savation lies within';
+		},
+		random:()=>{
+			return Math.random();
+		},
+		rollThreeDice:()=>{
+			return [1,2,3].map(_ => 1+Math.floor(Math.random()*6));
+		}
+	};
+
+	app.use('/graphqltypes', graphqlHttp({
+		schema:schemaTypes,
+		rootValue:rootTypes,
+		graphiql:true
+	}));
+}
+
+//demo3
+{
+	let schemaTypesArg = buildSchema(`
+		type Query {
+			rollDice(numDice: Int!, numSides: Int): [Int]
+		}
+	`);
+
+	var rootTypesArg = {
+		rollDice: function ({numDice, numSides=6}) {
+		var output = [];
+		for (var i = 0; i < numDice; i++) {
+			output.push(1 + Math.floor(Math.random() * (numSides)));
+		}
+		return output;
+		}
+	};
+
+
+	app.use('/graphqltypesarg', graphqlHttp({
+		schema:schemaTypesArg,
+		rootValue:rootTypesArg,
+		graphiql:true
+	}));
+}
+
+//demo4
+{
+
+	let schemaOjb = buildSchema(`
+		type RandomDie {
+			numSides: Int!
+			rollOnce: Int!
+			roll(numRolls:Int!):[Int]
+		}
+		type Query {
+			getDie(numSides:Int):RandomDie
+		}
+	`)
+
+	class RandomDie{
+		constructor(numSides=6){
+			this.numSides = numSides;
+		}
+		rollOnce(){
+			return 1+Math.floor(Math.random()*this.numSides);
+		}
+		roll({numRolls}){
+			let output = [];
+			for(let i=0;i<numRolls;i++){
+				output.push(this.rollOnce())
+			}
+			return output;
+		}
+	}
+
+	let rootObj = {
+		getDie:function({numSides}){
+			return new RandomDie(numSides);
+		}
+	}
+
+	//demo4
+	app.use('/graphqlobj', graphqlHttp({
+		schema:schemaOjb,
+		rootValue:rootObj,
+		graphiql:true
+	}));	
+}
+
+//demo5
+{
+	let schemaMutation = buildSchema(`
+		type Mutation {
+			setMessage(message:String):String
+		}
+
+		type Query {
+			getMessage:String
+		}
+	`)
+	let fakeDB = {};
+	let rootMutaion = {
+		setMessage:function({message}){
+			fakeDB.message = message;
+			return message;
+		},
+		getMessage:function(){
+			return fakeDB.message;
+		}
+	}
+	app.use('/graphqlmutation', graphqlHttp({
+		schema:schemaMutation,
+		rootValue:rootMutaion,
+		graphiql:true
+	}));	
+}
+
+//demo6
+{
+	let schemaMutationInput = buildSchema(`
+		input MessageInput {
+			content:String
+			author:String
+		}
+		type Message {
+			id:ID!
+			content:String
+			author:String
+		}
+
+		type Query {
+			getMessage(id:ID!):Message
+		}
+
+		type Mutation {
+			createMessage(input:MessageInput):Message
+			updateMessage(id:ID!, input:MessageInput):Message
+		}
+	`);
+	class Message {
+		constructor(id,{content, author}={}) {
+			this.id = id;
+			this.content = content;
+			this.author = author;
+		}
+	};
+	let fakeDBS = {};
+	let rootMutationInput = {
+		getMessage:function({id}){
+			if(!fakeDBS[id]) {
+				throw new Error(`no message with id:${id}`);
+			}
+			return new Message(id,fakeDBS[id]);
+		},
+		createMessage:function({input}){
+			var id = require('crypto').randomBytes(10).toString('hex');
+			fakeDBS[id] = input;
+			return new Message(id, input);
+		},
+		updateMessage:function({id,input}){
+			if(!fakeDBS[id]) {
+				throw new Error(`no message with id:${id}`);
+			}
+			fakeDBS[id] = input;
+			return new Message(id, input);
+		}
+	}
+	app.use('/graphqlmutationinput', graphqlHttp({
+		schema:schemaMutationInput,
+		rootValue:rootMutationInput,
+		graphiql:true
+	}));	
+}
+
 
 app.listen(4000);
 console.log("server is running at port 4000")
+
