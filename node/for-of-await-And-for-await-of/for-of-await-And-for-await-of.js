10713f329c4933e48caa4d41facd8408e8c126df
koroshi <stlcxc@163.com>
2019-06-25 15:19:06 +0800

    feat for await of

--- node/for-of-await-And-for-await-of/for-of-await-And-for-await-of.js
+++ node/for-of-await-And-for-await-of/for-of-await-And-for-await-of.js
@@ -44,9 +44,46 @@ async function test(){
         console.log(a)
     }
     console.timeEnd("startFour")//6s near[这样写会await 其中一个结束才开始第二个]
+
+ 
 }
-test();
+// test();
 
 //结论，
 //如果迭代对象是promise数组，异步迭代则for await of，for...of 里面 await【因为运行时间在函数执行的时候确定】
+//方法二和方法三时间由[3,2,1].map(sleep)这个决定已经三秒了，如果外部同步，里面异步就不一样了
+//如果没有依赖关系的，便利同步数据循环内部需要await异步，看看是否能转换成promise.all或者race来节约时间，不然每个互相等待上一个执行完才开始下一个
+
+
+async function test2(){
+       //例子二
+       console.log("test2 start")
+       async function* asyncGenerator() {
+        var i = 1;
+        while (i < 4) {
+            await sleep(i)
+          yield i++;
+        }
+      }
+      
+      console.time("timeExample2")
+      for await (num of asyncGenerator()) {
+        console.log(num);
+      }
+      console.timeEnd("timeExample2")
+
+    //   console.time("timeExample3")
+    //   for  (num of asyncGenerator()) {
+    //     console.log(await num);
+    //   }
+    //   console.timeEnd("timeExample3")
+    // 无法执行
+}
+// test().then(test2)
+test2()
+
+//结论，
+//如果迭代对象是promise数组，异步迭代则for await of，for...of 里面 await【因为运行时间在函数执行的时候确定】
+//方法二和方法三时间由[3,2,1].map(sleep)这个决定已经三秒了，如果外部同步，里面异步就不一样了
+//如果例子2形式，本身就是异步迭代就必须要for await...of,无法使用for...of await代替，并且时间也根据异步迭代器内部时间，这个例子里为6秒，分段拿到，不是6秒后拿到全部，类似外部同步for。。of内部await
 //如果没有依赖关系的，便利同步数据循环内部需要await异步，看看是否能转换成promise.all或者race来节约时间，不然每个互相等待上一个执行完才开始下一个
