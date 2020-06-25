9eecdc59478b0835aaf65d485acd52e644595d10
koroshi <stlcxc@163.com>
2020-06-25 17:54:28 +0800

    test fetch one page and save path

--- fetch_tilte_and_favtime/test.js
+++ fetch_tilte_and_favtime/test.js
@@ -1,16 +1,36 @@
 require('dotenv').config();
+const moment = require('moment');
+const fs =require('fs');
+const path = require('path');
 const axios = require('axios')
-const moment = require('moment')
 const getData = async function(){
-    const host = process.env.FETCH_HOST
+    const host = process.env.FETCH_HOST;
+    let fileName =''
     let response = await axios.get(host);
     if(response) {
-        console.log(response)
         response.data.data.medias.forEach(one => {
-            console.log(`title: ${one.title} and one fav_time : ${moment(one.fav_time * 1000).format('YYYY-MM-DD HH:mm')} /n`)
+            if (!fileName)  {
+                fileName = `${moment(one.fav_time * 1000).format('YYYY-MM-DD_HH_mm')}.txt`;
+                fs.writeFileSync(fileName, `${moment().format('YYYY-MM-DD HH:mm:ss')} \r\n\r\n\r\n`);
+            }
+
+            const appendData = `${one.title} \r\n收藏时间:${moment(one.fav_time * 1000).format('YYYY-MM-DD HH:mm')}  \r\n视频地址:${one.link} 
+ \r\n\r\n`;
+            fs.appendFileSync(fileName, appendData, 'utf8');
+            console.log(appendData)
         });
     } else {
         console.error('some err')
     }
+    if (process.env.SAVE_PATH) {
+        const readStream = fs.createReadStream(fileName)
+        const filePath = path.join(process.env.SAVE_PATH, fileName)
+        var writeStream = fs.createWriteStream(filePath);
+        readStream.pipe(writeStream);
+        readStream.on('end', function () {
+            fs.unlinkSync(fileName);
+        });
+
+    }
 };
-getData();
+getData().catch(console.log);
